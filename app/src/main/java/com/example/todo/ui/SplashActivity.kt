8c9ea2cbd4e54fdfc225c25e64a16a3068216931
xalam <ganufvyandana@gmail.com>
2021-09-06 15:35:54 +0700

    Splash

---
+++ app/src/main/java/com/example/todo/ui/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.example.todo.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.todo.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000)
+    }
+}
